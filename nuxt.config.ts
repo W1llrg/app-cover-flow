6e199b57d622b89cc54254817c7ddb47094155ee
Wallace <will.ragueneau@gmail.com>
2024-12-02 14:17:23 +0100

    fix stoopid db again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
     nitro: {
-        plugins: ['~/server/plugins/initDbAndCheckAdminOnStartup.js']
+        plugins: ['~/server/plugins/01.initDbAndCheckAdminOnStartup.js']
     },
 })
