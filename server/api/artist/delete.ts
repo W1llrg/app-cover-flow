e4e00c500a81b186d78e34e6562baa5192257a3e
Wallace <will.ragueneau@gmail.com>
2024-12-02 11:27:27 +0100

    api: CRUD operations for Artist

--- server/api/artist/delete.ts
+++ server/api/artist/delete.ts
@@ -1,6 +1,27 @@
-export default defineEventHandler((event) => {
-	return {
-		// TODO
-		post: 'delete an artist'
+import {AppDataSource} from "~/server/database/database";
+import Artist from "~/server/database/entities/Artist";
+import {readBody, createError} from "h3";
+
+export default defineEventHandler(async (event) => {
+	try {
+		const repo = AppDataSource.getRepository(Artist);
+		const body = await readBody(event);
+		const {id} = body;
+
+		if (!id) {
+			throw createError({
+				statusCode: 401,
+				statusMessage: "ID inconnu"
+			});
+		}
+
+		const artist = await repo.delete(id);
+
+		return {
+			statusCode: 200,
+			message: "Deleted successfully"
+		};
+	} catch (e: any) {
+		sendError(event, e);
 	}
 })
--- server/api/artist/update.ts
+++ server/api/artist/update.ts
@@ -1,6 +1,27 @@
-export default defineEventHandler((event) => {
-	return {
-		// TODO
-		post: 'update artist infos'
+import {AppDataSource} from "~/server/database/database";
+import Artist from "~/server/database/entities/Artist";
+import {createError, readBody, sendError} from "h3";
+
+export default defineEventHandler(async (event) => {
+	try {
+		const repo = AppDataSource.getRepository(Artist);
+		const body = await readBody(event);
+		const {name, description} = body;
+
+		if (!name || !description) {
+			throw createError({
+				statusCode: 401,
+				statusMessage: "Paramètres introuvables ou invalide"
+			});
+		}
+
+		const artist = await repo.update({name: name}, {description: description});
+
+		return {
+			statusCode: 200,
+			message: "insertion réussie"
+		};
+	} catch (e: any) {
+		sendError(event, e);
 	}
 })
