99fd741372258cec7672e997593158890dc92794
Wallace <will.ragueneau@gmail.com>
2024-12-04 10:49:11 +0100

    get all songs

--- server/api/song/getAll.ts
+++ server/api/song/getAll.ts
@@ -1,10 +1,10 @@
 import {AppDataSource} from "~/server/database/database";
-import Artist from "~/server/database/entities/Artist";
+import Song from "~/server/database/entities/Song";
 import {createError, readBody, sendError} from "h3";
 
 export default defineEventHandler(async (event) => {
 	try {
-		const repo = AppDataSource.getRepository(Artist);
+		const repo = AppDataSource.getRepository(Song);
 
 		const songs = await repo.find();
 
